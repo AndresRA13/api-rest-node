405350aab258f4f9bd4a9df809badde3fa02d46a
arktech365 <arktech365@gmail.com>
2025-12-02 10:03:40 -0500

    add middleware

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from "express";
 const app = express();
 
+app.use((req, res, next) => {
+    res.json({ "message" : "middleware Funcionando correctamente"});
+});
+
 app.get("/", (req, res) => {
-    res.send('hola mundo!');
+    res.json({ 'message': "Bienvenido a mi API REST con NodeJS y Firebase" });
 })
+
+import notFound from "./src/middlewares/not-found.js";
+
+app.use(notFound);
 
 const PORT = process.env.PORT || 3000;
 
